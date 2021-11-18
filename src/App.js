96ae480912809b8703c0bbad0f6cc8c50fae8a14
David <davidtorroija@gmail.com>
2021-11-18 10:56:47 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello David!</h1>
+      <h1>Hello Davidsito!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button
+        onClick={() => {
+          alert("hey!");
+        }}
+      >
+        {" "}
+        click me{" "}
+      </button>
     </div>
   );
 }
